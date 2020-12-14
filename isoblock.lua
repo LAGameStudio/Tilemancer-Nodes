10ce530b77a0eb3c1ff2e8759f0ac5e61a5f223f
h3rb <hgilliland@gmail.com>
2020-12-13 23:40:34 -0500

    Update isoblock.lua

--- isoblock.lua
+++ isoblock.lua
@@ -29,21 +29,21 @@ function apply()
 -- Fill background
 	r,g,b=getValue(3,x,y,1)
 	setPixel(0,x,y, r,g,b);
-	s = math.floor(tileSize/2 - 0.5)
+	s = tileSize/2 - 0.5
 -- Left side of cube
 	if (x >= sizeWidth and y > tileSize-sizeDepth) then
-		outr,outg,outb = getValue(0,x,y,1)
-		setPixel(0, s+((x-y)/2), s+math.ceil((x+y)/4)-Height, outr, outg, outb )
+		r,g,b = getValue(0,x,y,1)
+		setPixel(0, s+((x-y)/2), s+math.ceil((x+y)/4)-sizeHeight, r, g, b )
 	end
 -- Right side of cube
 	if (x <= sizeDepth and y < sizeHeight*2-1) then
-		outr,outg,outb = getValue(1,x,y,1)
-		setPixel(0, s+(x/2), ((y/2)-math.ceil(x/4))-Height, outr, outg, outb )
+		r,g,b = getValue(1,x,y,1)
+		setPixel(0, s+(x/2), ((y/2)-math.ceil(x/4))-sizeHeight, r, g, b )
 	end
 -- Top of cube
 	if (x <= sizeWidth and y < sizeHeight*2-1) then
-		outr,outg,outb = getValue(2,x,y,1)
-		setPixel(0, s-(x/2), ((y/2)-math.ceil(x/4))-Height, outr, outg, outb )
+		r,g,n = getValue(2,x,y,1)
+		setPixel(0, s-(x/2), ((y/2)-math.ceil(x/4))-sizeHeight, r, g, b )
 	end		
     end
 end
