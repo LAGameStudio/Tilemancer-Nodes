f829aa70ad073dd1c2b32dc1ae732c2aaa8afc16
h3rb <hgilliland@gmail.com>
2020-12-14 10:27:27 -0500

    Update border.lua

--- border.lua
+++ border.lua
@@ -11,15 +11,17 @@ end
  
 function apply()
     size = getTileSize()
-    thickness = getValue(2,0,0,1)/100.0
-    for i=0, size*size-1 do
-        x = i%tileSize
-        y = math.floor(i/tileSize)
-        if ( x <= thickness or y <= thickness or x >= size-thickness-1 or y >= size-thickness-1 ) then
-          r, g, b = getValue(0, x, y, 100.0) -- border texture
-        else
-          r, g, b = getValue(1, x, y, 100.0) -- fill texture
+    thickness = math.floor(getValue(2,0,0,1)/100.0 * size)
+    for x = 0, size -1 do
+        for y = 0, size -1 do
+            x = i%tileSize
+            y = math.floor(i/tileSize)
+            if ( x <= thickness or y <= thickness or x >= size-thickness-1 or y >= size-thickness-1 ) then
+              r, g, b = getValue(0, x, y, 100.0) -- border texture
+            else
+              r, g, b = getValue(1, x, y, 100.0) -- fill texture
+            end
+            setPixel(0, x, y, r, g, b)
         end
-        setPixel(0, x, y, r, g, b)
     end
 end
