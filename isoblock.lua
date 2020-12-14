28de182f9c3b219b2b2e8089c5aef7c4c80b42ff
h3rb <hgilliland@gmail.com>
2020-12-13 23:21:42 -0500

    Update isoblock.lua

--- isoblock.lua
+++ isoblock.lua
@@ -16,7 +16,7 @@ function apply()
     tileSize = getTileSize()
 	Height = math.floor(getValue(4,0,0,1)/100*tileSize-tileSize/2)
 	Width = math.floor(getValue(5,0,0,1)/100*tileSize)
-	Length = math.floor(getValue(6,0,0,1)/100*tileSize)
+	Depth = math.floor(getValue(6,0,0,1)/100*tileSize)
 -- Left side of cube
     for i=0, tileSize*tileSize-1 do
         x = i%tileSize
@@ -33,7 +33,7 @@ function apply()
     for i=0, tileSize*tileSize-1 do
         x = i%tileSize
         y = math.floor(i/tileSize)
-	if (x <= Length and y <= Height*2) then
+	if (x <= Depth and y <= Height*2) then
 		s = tileSize/2
 		outr,outg,outb = getValue(1,x,y,1)
 		setPixel(0, s+(x/2), ((y/2)-math.ceil(x/4))-Height, outr, outg, outb )
