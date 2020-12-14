12c1b1d53239eaf097779fba7dede36e83526fb4
h3rb <hgilliland@gmail.com>
2020-12-14 14:14:34 -0500

    Update circle.lua

--- circle.lua
+++ circle.lua
@@ -1,7 +1,7 @@
 -- freely gifted by lostastronaut.com
 function init()
-    setName("Circles")
-    setDesc("Generates Circles")
+    setName("Circle")
+    setDesc("Generates a Circle")
     setSize(108, 24+64+64+8)
     addOutput(24+32)
     addInputParameter("X Interval", "How often X", 24+64+16, 16, 1, 100)
@@ -13,13 +13,13 @@ function apply()
  size = getTileSize()
  x_i = getValue(1,0,0,1)
  y_i = getValue(2,0,0,1)
- rad = getValue(3,0,0,1)
+ cx = size/2
+ cy = size/2
+ radius = getValue(3,0,0,1)
  for x = 0, size -1 do
   for y = 0, size -1 do
-   cx = x % x_i
-   cy = y % y_i
    dist = math.sqrt( (cx - x)*(cx - x) + (cy - y)*(cy - y) )
-   if ( dist <= rad ) then
+   if ( dist <= radius ) then
     setPixel(0,x,y,1,1,1)
    else
     setPixel(0,x,y,0,0,0)
