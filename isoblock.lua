51786fd44935f83a36b81c388ba746664d6874d6
h3rb <hgilliland@gmail.com>
2020-12-13 23:17:12 -0500

    Update isoblock.lua

--- isoblock.lua
+++ isoblock.lua
@@ -6,7 +6,7 @@ function init()
 	addInput("z",24+8+8+8)
 	addInput("x",24+8+8+8+8)
 	addInput("y",24+8+8+8+8+8)
-	addInput("fi",24+8+8+8+8+8+8)
+	addInput("fi",24+8+8+8+8+8+8+8)
     addParameter("Depth", "Depth of the block", 24+64+8+8, 0, 0, 100)
     addParameter("Width", "Width of the block", 24+64+8+8+18, 100, 0, 100)
     addParameter("Length", "Length of the block", 24+64+8+8+18+18, 100, 0, 100)
@@ -17,35 +17,37 @@ function apply()
 	Depth = math.floor(getValue(4,0,0,1)/200*tileSize)
 	Width = math.floor(getValue(5,0,0,1)/100*tileSize)
 	Length = math.floor(getValue(6,0,0,1)/100*tileSize)
-	
+-- Left side of cube
     for i=0, tileSize*tileSize-1 do
         x = i%tileSize
         y = math.floor(i/tileSize)
 	r,g,b=getValue(3,x,y,1)
 	setPixel(0,x,y, r,g,b);
+	if (x >= tileSize-Width and y > tileSize-Length) then
 		s = tileSize/2
 		outr,outg,outb = getValue(0,x,y,1)
-		if (x >= tileSize-Width and y > tileSize-Length) then
-			setPixel(0, s+((x-y)/2), s+math.ceil((x+y)/4)-Depth, outr, outg, outb )
-		end
+		setPixel(0, s+((x-y)/2), s+math.ceil((x+y)/4)-Depth, outr, outg, outb )
+	end
     end
-	for i=0, tileSize*tileSize-1 do
+-- Right side of cube
+    for i=0, tileSize*tileSize-1 do
         x = i%tileSize
         y = math.floor(i/tileSize)
+	if (x <= Length and y < Depth*2) then
 		s = tileSize/2
 		outr,outg,outb = getValue(1,x,y,1)
-		if (x <= Length and y < Depth*2-1) then
-			setPixel(0, s+(x/2), ((y/2)-math.ceil(x/4))-Depth, outr, outg, outb )
-		end
+		setPixel(0, s+(x/2), ((y/2)-math.ceil(x/4))-Depth, outr, outg, outb )
+	end
     end
-	for i=0, tileSize*tileSize-1 do
+-- Top of cube
+    for i=0, tileSize*tileSize-1 do
         x = i%tileSize
         y = math.floor(i/tileSize)
+	if (x <= Width and y < Depth*2-1) then
 		s = tileSize/2
 		outr,outg,outb = getValue(2,x,y,1)
-		if (x <= Width and y < Depth*2-1) then
-			setPixel(0, s-(x/2), ((y/2)-math.ceil(x/4))-Depth, outr, outg, outb )
-		end
+		setPixel(0, s-(x/2), ((y/2)-math.ceil(x/4))-Depth, outr, outg, outb )
+	end
     end
 	
 end
